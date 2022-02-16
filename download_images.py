ecc230c719e28d498362fda598d57a0aafc8167e
Techyush <rickeydesai8@gmail.com>
2022-02-16 11:57:06 +0530

    Create download_images.py

---
+++ download_images.py
@@ -0,0 +1,61 @@
+from bs4 import *
+import requests
+import os
+
+
+def folder_create(images):
+    try:
+        folder_name = input("Create folder : ")
+        os.mkdir(folder_name)
+
+    except:
+        print("Folder exists with that name!")
+        folder_create()
+
+    download_images(images, folder_name)
+
+
+def download_images(images, folder_name):
+    count = 0
+
+    print(f"Total {len(images)} Image found!")
+
+    if len(images) != 0:
+        for i, image in enumerate(images):
+            try:
+                image_link = image["data-srcset"]
+            except:
+                try:
+                    image_link = image["data-fallback-src"]
+                except:
+                    try:
+                        image_link = image["src"]
+                    except:
+                        pass
+            try:
+                r = requests.get(image_link).content
+                try:
+                    r = str(r, 'utf-8')
+                except UnicodeDecodeError:
+                    with open(f"{folder_name}/images{i + 1}.jpg", "wb+") as f:
+                        f.write(r)
+                count += 1
+            except:
+                pass
+
+        if count == len(images):
+            print("All Images Downloaded")
+        else:
+            print(f"Total {count} Images Downloaded Out of {len(images)}")
+
+
+def main(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    images = soup.findAll('img')
+    folder_create(images)
+
+
+url = input("Enter Input URL: ")
+
+main(url)
