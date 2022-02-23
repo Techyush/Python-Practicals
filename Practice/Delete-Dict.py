3a58c6e1a773b7f001bddea44898a3af5041f987
dharmeshvyas <72197943+Techyush@users.noreply.github.com>
2022-02-23 12:23:45 +0530

    New files

---
+++ Practice/Delete-Dict.py
@@ -0,0 +1,11 @@
+my_dict = {'name': 'Jack', 'age': 26}
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+del dict['Name']
+print("name Delete after dictionary : ", dict)
+
+dict.clear()
+print("Dictionary now  clear ", dict)
+del dict
+print("Dictionary is deleted ", dict)
