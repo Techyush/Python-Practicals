6c911d25a1c525ad87ef87fb558dae3a569f3153
dharmeshvyas <72197943+Techyush@users.noreply.github.com>
2022-03-07 12:14:53 +0530

    New files

---
+++ Practice/Update-Dict.py
@@ -0,0 +1,6 @@
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8
+print("Before New Record Insert", dict)
+dict['School'] = "DPS School"
+print("After nsert new Record ", dict)
